dd59697bdd4fe7767d42565cd2bc01cd8a55b5fd
Toni Cafiero <tcafiero@hotmail.com>
2019-03-28 19:28:34 +0100

    work in progress

--- Stages/Building/Build.cmd
+++ Stages/Building/Build.cmd
@@ -1,5 +1,5 @@
 @echo off
-"C:\Program Files\MATLAB\R2017b\bin\matlab.exe" -nosplash -softwareopengl -nodesktop -minimize -sd "%cd%\Source\Code" folder -r "generate" -logfile .\logfile.log | more
+"C:\Program Files\MATLAB\R2019a\bin\matlab.exe" -nosplash -softwareopengl -nodesktop -minimize -sd "%cd%\Source\Code" folder -r "generate" -logfile .\logfile.log | more
 :loop
 tasklist /fi "imagename eq MATLAB.exe" |find ":" > nul
 if errorlevel 1 goto loop
