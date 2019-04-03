036d45b142258abfcfd85d187c3f7668593b2ba6
Toni Cafiero <tcafiero@hotmail.com>
2019-04-03 15:58:52 +0200

    work in progress

--- Source/TestCode/TestCase.cmd
+++ Source/TestCode/TestCase.cmd
@@ -1,6 +1,6 @@
 rem @echo off
-cd "%~dp0"
-node "%~dp0 HILgateway.js" COM52 5000 500 10
+rem cd "%~dp0"
+node ".\Source\TestCode\HILgateway.js" COM52 5000 500 10
 rem node "%~dp0\HILgateway.js" COM52 5000 500 10
 IF %ERRORLEVEL% == 1 (
    echo HIL Test Not Passed
